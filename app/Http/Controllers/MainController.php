5255cf2482ab2e13405478291d4119440160800f
Nemanja <nemanja.d.krstic@gmail.com>
2015-12-10 16:06:43 +0100

    read sample rtf and return plain text

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use RTFLex\io\StreamReader;
+use RTFLex\tree\RTFDocument;
 use Illuminate\Http\Request;
+use RTFLex\tokenizer\RTFTokenizer;
 use App\Http\Controllers\Controller;
 
 class MainController extends Controller
 {
     public function getIndex()
     {
-    	return view('welcome');
+        $documentPath = public_path('uploads/sample.rtf');
+
+        $reader = new StreamReader($documentPath);
+        $tokenizer = new RTFTokenizer($reader);
+        $document = new RTFDocument($tokenizer);
+
+        $text = $document->extractText();
+
+        return $text;
     }
 }
