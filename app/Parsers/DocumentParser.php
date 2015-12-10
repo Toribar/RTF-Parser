bf8f312274ec73d5a53533a5c3404c321fc5c356
Nemanja <nemanja.d.krstic@gmail.com>
2015-12-10 16:17:37 +0100

    create overview for document parser class

--- app/Parsers/DocumentParser.php
+++ app/Parsers/DocumentParser.php
@@ -2,7 +2,51 @@
 
 namespace App\Parsers;
 
+/**
+ * Class for parsing given text with given rules.
+ */
 class DocumentParser
 {
-	//
+    /**
+     * Key-value array to hold all our found matches.
+     * Key is the string provided for each rule.
+     * Value is parsed text.
+     *
+     * @var array
+     */
+    private $matches = [];
+
+    /**
+     * Create new parser object.
+     *
+     * @param string $text Text to be parsed
+     * @param array $rules Rules to apply when parsing text.
+     */
+    public function __construct($text, array $rules)
+    {
+        $this->parse($text, $rules);
+    }
+
+    /**
+     * Parse given text with given rules.
+     *
+     * @return void
+     */
+    private function parse($text, array $rules)
+    {
+        // 1. Create regex pattern for all rules.
+        // 2. Extract regex matches.
+        // 3. Fill $matches field with found values for each given rule key.
+    }
+
+    /**
+     * Get parsed value for given rule key.
+     *
+     * @param  string $key Key from rules array.
+     * @return string
+     */
+    public function get($key)
+    {
+        //
+    }
 }
